fa5df6d8b09fec09cd85022cc24e3154381c4d09
Muhammad Rezky Ananda <138985405+mrezkya@users.noreply.github.com>
2023-07-13 11:39:06 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+let a = ['dion', 'panini', 'rezky', 'aska']
+
+for (let i = 0; i < a.length; i++) {
+    console.log(a[i]);
+}
+
+
+let b = (nama) => {
+    alert(`Halo ${nama}`);
+}
+b('Muhammad Rezky Ananda');
+
+const c = 5;
+(c == 5) ? console.log('hi'): console.log('Not HI');
